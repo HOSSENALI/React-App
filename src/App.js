30830600fcc53bbf22dc6adf68d9cc6934d61b46
Hossen Ali <hossen11@cse.pstu.ac.bd>
2022-05-18 20:35:06 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const parent = "AppParent";
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  }
+  const decrement = () => {
+    setCounter(counter - 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='counter'>
+        <button onClick={increment}>+</button>
+       {counter}
+        <button onClick={decrement}>-</button>
+      </div>
+      <h1>Welcomw to my site</h1>
+      <Child parent={parent} />
     </div>
   );
 }
-
+function Child(props) {
+  return (
+    <h1>Hey,I am a child,my parent is {props.parent}</h1>
+  );
+}
 export default App;
