0f73e2e9829e916b82bd028e1509c5384761af24
Hossen Ali <hossen11@cse.pstu.ac.bd>
2022-05-19 10:18:08 +0200

    createComponent

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import React, { useState } from 'react';
 function App() {
   const parent = "AppParent";
   const [counter, setCounter] = useState(0);
+  const [bgColor, setBgColor] = useState(false);
+  const [formValue, setFormValue] = useState("");
 
   const increment = () => {
     setCounter(counter + 1);
@@ -12,16 +14,29 @@ function App() {
   const decrement = () => {
     setCounter(counter - 1);
   }
+  const changeColor = () => {
+    setBgColor(!bgColor);
+  }
+  const formHandle = (e) => {
+    setFormValue(e.target.value);
+    
+  }
+
   return (
-    <div className="App">
-      <div className='counter'>
-        <button onClick={increment}>+</button>
-       {counter}
-        <button onClick={decrement}>-</button>
+    <>
+      <div className={bgColor ? "red" : "App"}>
+        <div className='counter'>
+          <button onClick={increment}>+</button>
+          {counter}
+          <button onClick={decrement}>-</button>
+        </div>
+        <button onClick={changeColor}>Change Color</button>
+        <h1>Welcomw to my site</h1>
+        <Child parent={parent} />
       </div>
-      <h1>Welcomw to my site</h1>
-      <Child parent={parent} />
-    </div>
+      <input type="text" value={formValue} onChange={formHandle} />
+      <h2>Form Data is:{formValue}</h2>
+    </>
   );
 }
 function Child(props) {
